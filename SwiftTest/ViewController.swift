bad16f235a1ead2b42e06d0936e8a45b13970ccd
井川　雅央 <masaoikawa@ikawa-iMac.local>
2014-06-14 01:19:47 +0900

    Initial Commit

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by 井川　雅央 on 2014/06/14.
+//  Copyright (c) 2014年 MASAO IKAWA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
